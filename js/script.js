21eccceb2afb55e83e39ee3e5cecbf953a0fb0cd
rachel-oromo <124389469+rachel-oromo@users.noreply.github.com>
2023-05-30 02:19:58 +0000

    sync changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Rachel Oromo
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates fahrenheit to celsius.
+ */
+function enterClicked() {
+  //input
+  const fahrenheittemp = parseFloat(
+    document.getElementById("fahrenheittemp").value
+  )
+
+  //process
+  const celsiustemp = ((fahrenheittemp - 32) * 5) / 9
+
+  //output
+  document.getElementById("celsiustemp").innerHTML =
+    "The temperture in celsius is: " + celsiustemp.toFixed(2) + " °C."
+}
